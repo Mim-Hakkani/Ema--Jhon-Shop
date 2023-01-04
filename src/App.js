bb3d29fad15037e514a138b4d081cd7b723d888b
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-04 19:03:04 +0600

    react router basic setup and navigation link is ready to use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Error from './components/Error/Error';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import Order from './components/Order/Order';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+        <Header></Header>
+      <Routes>
+        <Route path="/" element={<Shop/>}></Route>
+        <Route path="/shop" element={<Shop/>}></Route>
+        <Route path="/order" element={<Order/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/inventory" element={<Inventory/>}></Route>
+        <Route path="*" element={<Error/>}></Route>
+      </Routes>
+    sdsad
     </div>
   );
 }
---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Order = () => {
+    return (
+        <div>
+            <p>This is the Order </p>
+        </div>
+    );
+};
+
+export default Order;
