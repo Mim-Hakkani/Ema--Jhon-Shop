e70dc88084950f510c644f21a772a7e3c903a683
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-01-04 19:27:39 +0600

    create custom hooks

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import UseProducts from '../hooks/UseProducts';
 
 const Order = () => {
+    const [products,setProducts] =UseProducts();
+  console.log('products :: ',products);
     return (
         <div>
-            <p>This is the Order </p>
+            <p>This is the Order {products.length}</p>
         </div>
     );
 };
